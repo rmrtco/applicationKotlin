27d08059e7b571eb9d0916772cecf3f24faad76b
Rakshith M R <rakshithmrco@gmail.com>
2020-04-18 13:22:08 +0530

    first commit

---
+++ app/src/main/java/site/rakshith/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package site.rakshith.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
